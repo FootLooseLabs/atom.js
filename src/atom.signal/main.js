d1eea3c23e697bf0e198deac994cd66a521d66b6
Apratim <apratim.ankur@gmail.com>
2021-01-29 05:51:10 +0530

    logging bugifx in signal/main

--- src/atom.signal/main.js
+++ src/atom.signal/main.js
@@ -223,7 +223,7 @@ AtomSignal.publishToInterface = async (interfaceLabel, message, paramList) => {
 		throw e;
 	}
 
-	console.debug("DEBUG: AtomSignal: Successfully Established - \n", establishedSignalStatus.getLabel());
+	console.debug("DEBUG: AtomSignal: Successfully Established - \n", establishedSignalStatus.signal.getLabel());
 
 	return new Promise(async (resolve, reject)=>{
 		var signal = establishedSignalStatus.signal;
@@ -282,7 +282,7 @@ AtomSignal.subscribeToInterface = async (interfaceLabel) => {
 		throw e;
 	}
 
-	console.debug("DEBUG: AtomSignal: Successfully Established - \n", establishedSignalStatus.getLabel());
+	console.debug("DEBUG: AtomSignal: Successfully Established - \n", establishedSignalStatus.signal.getLabel());
 
 	return new Promise(async (resolve, reject)=>{
 
